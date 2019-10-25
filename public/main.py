2224f639a51bf464664c475be814bcdb5a3a3109
Qasim Wani <qasim31wani@gmail.com>
2019-10-25 14:33:06 -0400

    Added root child_process

--- public/main.py
+++ public/main.py
@@ -1,44 +1,50 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
-#Requires the “PyPDF2” and “OS” modules to be imported
-import os, PyPDF2
-
-#Ask user where the PDFs are
-userpdflocation = input('Folder path to PDFs that need merging:')
-
-#Sets the scripts working directory to the location of the PDFs
-os.chdir(userpdflocation)
-
-#Ask user for the name to save the file as
-userfilename=input('What should I call the file?')
-
-#Get all the PDF filenames
-pdf2merge = []
-for filename in os.listdir('.'):
-    if filename.endswith('.pdf'):
-        pdf2merge.append(filename)
-
-pdfWriter = PyPDF2.PdfFileWriter()
-
-#loop through all PDFs
-for filename in pdf2merge:
-    #rb for read binary
-    pdfFileObj = open(filename,'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    #Opening each page of the PDF
-    for pageNum in range(pdfReader.numPages):
-        pageObj = pdfReader.getPage(pageNum)
-        pdfWriter.addPage(pageObj)
-#save PDF to file, wb for write binary
-pdfOutput = open(userfilename+'.pdf', 'wb')
-#Outputting the PDF
-pdfWriter.write(pdfOutput)
-#Closing the PDF writer
-pdfOutput.close()
+from fpdf import FPDF
+import os
+import sys
+import subprocess
+pdf = FPDF('P','mm','A4')
+# imagelist is the list with all image filenames
+x,y,w,h = 0,0,200,250
+for image in sys.argv[0]:
+    pdf.add_page()
+    pdf.image(image,x,y,w,h)
+pdf.output("yourfile.pdf", "F")
 
 
-# %%
+# PDF to Word
+
+"""
+# pdf source file(s) and target paths
+basedir = 'C:/path/to'
+pdfdir = os.path.normpath(basedir + '/pdf')
+docdir = os.path.normpath(basedir + '/doc')
+docxdir = os.path.normpath(basedir + '/docx')
+
+# absolute path to libre office writer application
+lowriter = 'C:/Progra~2/LibreO~1/program/swriter.exe'
+
+# output-filter for conversion
+#outfilter = ':"Office Open XML Text"'
+#outfilter = ':"MS Word 2003 XML"'
+#outfilter = ':"MS Word 2007 XML"'
+#outfilter = ':"MS Word 97"'
+outfilter = ''
+
+i = 0
+for top, dirs, files in os.walk(pdfdir):
+    for filename in files:
+        if filename.endswith('.pdf'):
+            i = i + 1
+            abspath_pdf = os.path.normpath(os.path.join(top, filename))
+
+            print 'Converting {0} into .doc format..'.format(abspath_pdf)
+            subprocess.call('{0} --invisible --convert-to doc{1} --outdir "{2}" "{3}"'
+                            .format(lowriter, outfilter, docdir, abspath_pdf), shell=True)
+
+            print 'Converting {0} into .docx format..'.format(abspath_pdf)
+            subprocess.call('{0} --invisible --convert-to docx{1} --outdir "{2}" "{3}"'
+                            .format(lowriter, outfilter, docxdir, abspath_pdf), shell=True)
+
+    print '|-------------------------------------------------------|'
+    print 'Done. Converted {0} pdf files.'.format(i)
+"""
